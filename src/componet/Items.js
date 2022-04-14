8c72fe0b688cbf62c2eb17f43cd51e767cd3c766
ranashivam207 <ranashivam207@gmail.com>
2022-04-14 13:57:31 +0530

    Showing news itmes

--- src/componet/Items.js
+++ src/componet/Items.js
@@ -2,22 +2,19 @@ import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 
 export class Items extends Component {
-    constructor(){
-        super();
-        console.log("hello");
-    }
-    static propTypes = {}
+   
+    
 
     render() {
-    let {title, discription, imageurl, url} = this.props;
+    let {title, discription, imageurl, newsurl} = this.props;
         return (
             <div className="my-3">
                 <div className="card" style={{width: "18rem"}}>
                     <img src={imageurl} className="card-img-top" alt="..." />
                     <div className="card-body">
-                                <h5 className="card-title"> {title}</h5>
+                                <h5 className="card-title"> {title}...</h5>
                         <p className="card-text">{discription}</p>
-                        <a href={this.url} className="btn btn-primary btn-sm">Read More</a>
+                        <a href={newsurl} target="_blank" className="btn btn-primary btn-sm">Read More</a>
                     </div>
                 </div>
             </div>
--- src/componet/Navbar.js
+++ src/componet/Navbar.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 
 export class Navbar extends Component {
-  static propTypes = {}
+ 
 
   render() {
     return (
