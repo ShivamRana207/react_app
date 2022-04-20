6bfc74d3e8776182ce2233886ada974ccb03d187
ranashivam207 <ranashivam207@gmail.com>
2022-04-20 10:15:22 +0530

    using update function to reducing code

--- src/componet/Content.js
+++ src/componet/Content.js
@@ -11,9 +11,9 @@ export class Content extends Component {
     static propTypes = {
         country: PropTypes.string,
         category: PropTypes.string,
-        PageSize: PropTypes.number 
+        PageSize: PropTypes.number
     }
-            constructor() {
+    constructor() {
         super();
 
         this.state = {
@@ -23,10 +23,10 @@ export class Content extends Component {
 
         }
     }
-    async componentDidMount() {
 
+    async updateNews() {
         let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c9233111fe6c4dafa7f796e345798c9f&page=${this.state.page}&pageSize=${this.props.PageSize}`;
-        this.setState({loading: true})
+        this.setState({ loading: true })
         let data = await fetch(url);
         let parsedData = await data.json();
         console.log(parsedData);
@@ -37,6 +37,9 @@ export class Content extends Component {
             totalResults: parsedData.totalResults
 
         });
+    }
+    async componentDidMount() {
+        this.updateNews();
 
     }
     handlenext = async () => {
@@ -44,46 +47,30 @@ export class Content extends Component {
 
         }
         else {
-
-            console.log(" handlenext clicked")
-            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c9233111fe6c4dafa7f796e345798c9f&page=${this.state.page + 1}&pageSize=${this.props.PageSize}`;
-            this.setState({loading: true})
-            let data = await fetch(url);
-            let parsedData = await data.json();
-
+            this.updateNews();
             this.setState({
-                loading: false,
                 page: this.state.page + 1,
-                articles: parsedData.articles
             });
         }
+
     }
     handlepre = async () => {
-
-
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c9233111fe6c4dafa7f796e345798c9f&page=${this.state.page - 1}&pageSize=${this.props.PageSize}`;
-        this.setState({loading: true})
-        let data = await fetch(url);
-        let parsedData = await data.json();
-        
+        this.updateNews();
         this.setState({
-            loading: false,
             page: this.state.page - 1,
-            articles: parsedData.articles
         });
-
     }
     render() {
 
         return (
             <div className="container my-4">
                 <h2 >Top Content</h2>
-                {this.state.loading && <Spiner className="text-center"/>}
+                {this.state.loading && <Spiner className="text-center" />}
 
                 <div className="row">
-                    { this.state.articles.map((element) => <div className="col-md-auto" key={element.url}>
-                        <Items title={element.title.slice(0, 40)} imageurl={element.urlToImage} newsurl={element.url} auther={element.author} date={element.publishedAt.slice(0,10)}
-                        source={element.source.name} />
+                    {this.state.articles.map((element) => <div className="col-md-auto" key={element.url}>
+                        <Items title={element.title.slice(0, 40)} imageurl={element.urlToImage} newsurl={element.url} auther={element.author} date={element.publishedAt.slice(0, 10)}
+                            source={element.source.name} />
                     </div>)}
                 </div>
                 <div className="container d-flex justify-content-between">
@@ -100,3 +87,9 @@ export class Content extends Component {
 }
 
 export default Content
+
+
+
+
+
+
